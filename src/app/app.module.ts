bd4af8980106f85cb75f8fa6ac3401f88be31602
Andressa Litwinski <andressalitwinski@gmail.com>
2024-05-04 18:52:45 +0100

    header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AddMemberComponent } from './components/members/add-member/add-member.component';
+import { AddItemComponent } from './components/itens/add-item/add-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AddMemberComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
